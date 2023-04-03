18cf248d74e8b4c6c1fa8ba4724a3cd41eb5beec
Sergio Veliz <>
2023-04-03 18:25:14 +0300

    initital

---
+++ TraderJo/TraderJoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraderJoApp.swift
+//  TraderJo
+//
+//  Created by Sergio Veliz on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TraderJoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
